cfed57fbd853af1cd6230dbec2460fc3d9c9840b
Shane Avery Sistoza <ss2010@hawaii.edu>
2018-01-23 09:44:04 -0500

    added CrossOrigin.

--- src/main/java/com/revature/ScrumHub/rest/BoardCtrl.java
+++ src/main/java/com/revature/ScrumHub/rest/BoardCtrl.java
@@ -3,6 +3,7 @@ package com.revature.ScrumHub.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import com.revature.ScrumHub.service.BoardService;
 
 @RestController
 @RequestMapping("/board/")
+@CrossOrigin(origins="*")
 public class BoardCtrl {
 
 	@Autowired
--- src/main/java/com/revature/ScrumHub/rest/ScrumUserCtrl.java
+++ src/main/java/com/revature/ScrumHub/rest/ScrumUserCtrl.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,7 +21,8 @@ import com.revature.ScrumHub.bean.ScrumUser;
 import com.revature.ScrumHub.service.ScrumUserService;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/user/")
+@CrossOrigin(origins="*")
 public class ScrumUserCtrl {
 
 	@Autowired
