a1da608ef0e12f30877fc2e574f9adcbdda2a47c
Francisco Palomino <fjpm03@gmail.com>
2018-01-30 11:42:31 -0500

    Added orderby board id to the Scrum user bean

--- src/main/java/com/revature/ScrumHub/bean/ScrumUser.java
+++ src/main/java/com/revature/ScrumHub/bean/ScrumUser.java
@@ -11,6 +11,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -42,6 +43,7 @@ public class ScrumUser implements Serializable{
 	private String lastname;
 	
 	@OneToMany(mappedBy="boardMemberId", fetch=FetchType.EAGER)
+	@OrderBy("sboard")
 	private Set<BoardMember> associatedBoards = new HashSet<>();
 	
 	public ScrumUser() {}
